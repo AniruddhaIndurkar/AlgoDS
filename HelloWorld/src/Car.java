51ba456b079146edabf47ed99b60c8de60a2062d
Andy <33083630+aniruddhaindurkar@users.noreply.github.com>
2021-09-16 10:04:34 +1000

    - Create a simple class to test the project.

---
+++ HelloWorld/src/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+    private String car;
+    public Car(String car){
+        this.car = car;
+    }
+
+    public String getCar(){
+        return this.car;
+    }
+
+    public void setCar(){
+        this.car = car;
+    }
+}
